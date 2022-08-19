838c982fb0ff32437119d12af086dbcd54de4331
Adrian <guzniczak.adrian@gmail.com>
2022-08-19 15:44:41 +0200

    site_core application has been added!

--- photo_renovation_website/urls.py
+++ photo_renovation_website/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unittest.mock import patch
 from django.contrib import admin
 from django.urls import path
+from site_core.views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
 ]
